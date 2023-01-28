24556505c5905503aff2675e1138d6e7886b4649
David D <87215831+davidinfosec@users.noreply.github.com>
2023-01-28 04:39:49 -0500

    Help menu and minor fixes

--- dnn.py
+++ dnn.py
@@ -5,11 +5,13 @@ import subprocess
 import os.path
 import pyperclip
 import sys
+import time
+random.seed(time.time())
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-TLD", "--TLD", help="Domain extension/TLD", default=".com, .org, .co", type=str)
-parser.add_argument("-sP", "--savePath", help="Path to save the generated domain list", default='C:/Users/David/Desktop/David Domain Finder V3/Results/', type=str)
-parser.add_argument("-wlP", "--wordListPath", help="Path to wordlists directory", default='C:/Users/David/Desktop/David Domain Finder V3/wLists/', type=str)
+parser.add_argument("-sP", "--savePath", help="Path to save the generated domain list", default='Results', type=str)
+parser.add_argument("-wlP", "--wordListPath", help="Path to wordlists directory", default='wLists', type=str)
 parser.add_argument("-L1", "--wordList1", help="Path to wordlist 1", default='sampleList1.txt', type=str)
 parser.add_argument("-L2", "--wordList2", help="Path to wordlist 2", default='sampleList2.txt', type=str)
 args = parser.parse_args()
@@ -49,10 +51,12 @@ with open(completeName, 'w') as f:
             resultA = (random.choice(open(WordList1).read().split()).strip())
             resultB = (random.choice(open(WordList2).read().split()).strip())
             randomOrder = random.choice("AB")
+            if (count > 1):
+                f.write('\n')
             if (randomOrder == "A"):
-                print(resultA + resultB + resultTLD, file=f)
+                f.write(resultA + resultB + resultTLD)
             else:
-                print(resultB + resultA + resultTLD, file=f)
+                f.write(resultB + resultA + resultTLD)
 
 
     #prints and writes a console/file completion message
@@ -61,9 +65,6 @@ with open(completeName, 'w') as f:
     #executes function
     combineLists()
 
-# clipboard.copy(fo)
-pyperclip.copy(open(completeName, 'r').read())
-
 # Open the file for writing in the beginning
 with open(completeName, 'w') as f:
     f.write("WordList1 and WordList2 have been combined and copied to your clipboard with your preferred TLD(s): " + ', '.join(TLDs) + '\n\n')
@@ -71,23 +72,27 @@ with open(completeName, 'w') as f:
     f.write("Check their availability at https://www.name.com/names using the bulk search utility, sort by price, and go! \n")
     f.write("Be sure to check out DavidInfosec.com for more awesome tools! " + '\n\n')
     f.write("Your generated domains:\n")
-    
-    def combineLists():
-        count = 0
-        while (count < 500):
-            count = count + 1
-            resultTLD = (random.choice(TLDs))
-            resultA = (random.choice(open(WordList1).read().split()).strip())
-            resultB = (random.choice(open(WordList2).read().split()).strip())
-            randomOrder = random.choice("AB")
-            if (count > 1):
-                f.write('\n')
-            if (randomOrder == "A"):
-                f.write(resultA + resultB + resultTLD)
-            else:
-                f.write(resultB + resultA + resultTLD)
-
     combineLists()
     subprocess.Popen(completeName, shell=True)
     
-    
+    # clipboard.copy(fo)
+pyperclip.copy(open(completeName, 'r').read())
+
+def help():
+    print("Usage: python dnn.py [options]\n")
+    print("Options:")
+    print("-TLD, --TLD\t\tDomain extension/TLD (default: '.com, .org, .co')")
+    print("-sP, --savePath\t\tPath to save the generated domain list (default: 'Results')")
+    print("-wlP, --wordListPath\tPath to wordlists directory (default: 'wLists')")
+    print("-L1, --wordList1\t\tPath to wordlist 1 (default: 'sampleList1.txt')")
+    print("-L2, --wordList2\t\tPath to wordlist 2 (default: 'sampleList2.txt')")
+    print("-h, --help\t\tShow this help menu\n")
+    print("Example: python dnn.py -TLD .com -sP my_results -wlP my_wordlists -L1 list1.txt -L2 list2.txt")
+
+
+
+if __name__ == "__main__":
+    if "-h" in sys.argv or "--help" in sys.argv:
+        help()
+    else:
+        main()
